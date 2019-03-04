b53eb7ef0ead4fe8dd2afcd20981bd0961805612
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-03-04 23:00:44 +0100

    Replaced console.log to logger

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const server = require('http').createServer(handler)
 const io = require('socket.io')(server)
 
+const logger = require('./logger')
+const config = require('./config.json')
+
 function handler(request,response) {
 	response.end('Hello World')
 }
 
 io.on('connection', (socket) => {
-    console.log('Polaczenie')
-    socket.emit('connected',{msg: 'Polaczono z serwerem'})
-    
-    socket.on('message', (data) =>{
-        socket.emit('message', {msg: 'Hello from Socket.io'})
-        console.log(data)
-    })
-})
+	socket.emit('connected',{msg: 'Polaczono z serwerem'})
 
-server.listen(3000)
+	socket.on('message', (data) =>{
+		socket.emit('message', {msg: 'Hello from Socket.io'})
+		logger.info(data)
+	})
+})
+const serverPort = config['server']['port']
+server.listen(serverPort)
+logger.info('Server started on port '+serverPort)
