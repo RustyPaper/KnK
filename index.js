97ed77f95483fb4ac4751c8ff22eba1eac9ea0b3
MarcinGrzeszczak <marcin.grzeszczak@gmail.com>
2019-03-04 21:50:05 +0100

    Implemented basic socket connection

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const http = require('http')
+const server = require('http').createServer(handler)
+const io = require('socket.io')(server)
 
-const server=http.createServer((request,response)=>{
+function handler(request,response) {
 	response.end('Hello World')
+}
+
+io.on('connection', (socket) => {
+    console.log('Polaczenie')
+    socket.emit('connected',{msg: 'Polaczono z serwerem'})
+    
+    socket.on('message', (data) =>{
+        socket.emit('message', {msg: 'Hello from Socket.io'})
+        console.log(data)
+    })
 })
 
 server.listen(3000)
